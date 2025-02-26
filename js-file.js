8aa5f6968111bb33b4e4af22699170cb499b9b29
annalast1 <166384111+annalast1@users.noreply.github.comm>
2025-02-26 18:13:26 +0000

    Basic layout html and css Button displays dialog box js - no styling

---
+++ js-file.js
@@ -0,0 +1,25 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+      return (`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`); 
+    }
+  }
+
+/* const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet") */
+
+const myLibrary = [];
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+const card = document.querySelector(".card");
+const dialog = document.querySelector("dialog")
+const bookForm = document.querySelector("#book-info");
+bookForm.addEventListener("click", () => {
+    dialog.showModal();
+})
