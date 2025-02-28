6fe3481296fa98d222be28fb6eae2e4e3f10b95c
annalast1 <166384111+annalast1@users.noreply.github.comm>
2025-02-28 17:29:49 +0000

    Close button added to dialog box JS to add book, clear form completed

--- js-file.js
+++ js-file.js
@@ -1,3 +1,18 @@
+// DOM elements
+const card = document.querySelector(".card");
+const dialog = document.querySelector("dialog");
+const bookForm = document.querySelector("#book-info");
+const close = document.querySelector("#close");
+const addBook = document.querySelector("#add");
+const form = document.querySelector("#form");
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let pages = document.getElementById("pages");
+let read = document.getElementById("read");
+
+const myLibrary = [];
+
+// Book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,22 +23,33 @@ function Book(title, author, pages, read) {
     }
   }
 
-/* const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet") */
-
-const myLibrary = [];
-
 function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
 
-const card = document.querySelector(".card");
-const dialog = document.querySelector("dialog")
-const bookForm = document.querySelector("#book-info");
+function reset() {
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.value = '';
+}
+
+close.addEventListener("click", () => {
+  dialog.close();
+  reset();
+})
+
 bookForm.addEventListener("click", () => {
     dialog.showModal();
 })
-const addBook = Document.querySelector("#add");
+
 addBook.addEventListener("click", () => {
-  
+    title = title.value;
+    author = author.value;
+    pages = pages.value;
+    read = read.value;
+    console.log(title);
+    addBookToLibrary(title, author, pages, read);
 })
+
